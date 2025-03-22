9e4e81604dd7e750b917c70deed5b7a7671c7ee7
Pranav <pranav6112004@gmail.com>
2025-03-23 01:56:04 +0530

    22nd march

--- 22_03_25.cpp
+++ 22_03_25.cpp
@@ -1,42 +1,72 @@
 #include<iostream>
-#include<vector>
 #include<unordered_map>
-
+#include<queue>
 using namespace std;
-
 
 class Solution {
     public:
-        vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-            //  do baar loop chalana hai kyuki baad mai koi dish ban gyi to pahele vali bhi ban jayegi;
-            vector<string>ans;
-            unordered_map<string,bool>pre;
-            for(auto i : supplies){
-                pre[i] = true;
+      
+        int countCompleteComponents(int n, vector<vector<int>>& edges) {
+            int count = 0;
+            unordered_map<int,vector<int>>adj;
+            vector<int>indegree(n,0);
+            for(int i=0;i<edges.size();i++){
+                int f = edges[i][0];
+                int s = edges[i][1];
+                adj[f].push_back(s);
+                adj[s].push_back(f);
             }
-            bool a = true;
-            while(a){
-                a = false;
-            for(int i=0;i<recipes.size();i++){
-                string make = recipes[i];
-                bool check = false;
-                if(pre.find(make)!=pre.end()){
-                    continue;
-                }
-                for(int j=0;j<ingredients[i].size();j++){
-                    if(pre.find(ingredients[i][j]) == pre.end()){
-                        check = true;
-                        break;
+            vector<vector<int>>comp;
+            unordered_map<int,bool>vis;
+            queue<int>q;
+            bool check = false;
+            vector<int>store;
+            for(int i=0;i<n;i++){
+                if(!vis[i]){
+                    if(check){
+                        comp.push_back(store);
+                        check = false;
+                        store.clear();
+                    }
+                    vis[i] = true;
+                    store.push_back(i);
+                    q.push(i);
+                    check = true;
+                    while(!q.empty()){
+                        auto top = q.front();
+                        q.pop();
+    
+                        for(auto j : adj[top]){
+                            indegree[j]++;
+                            if(!vis[j]){
+                                vis[j] = true;
+                                q.push(j);
+                                store.push_back(j);
+                            }
+                        }
                     }
                 }
-                if(!check){
-                pre[make] = true;
-                ans.push_back(make);
-                a = true;
+            }
+    
+            if(!store.empty()){
+                comp.push_back(store);
+                store.clear();
+            }
+           
+           for(int i=0;i<comp.size();i++){
+            bool check = true;
+            for(int j=0;j<comp[i].size();j++){
+                int length = comp[i].size();
+                if(indegree[comp[i][j]]!=comp[i].size()-1){
+                    check = false;
                 }
             }
+            if(check){
+                count++;
             }
-            vector<string> anss = {ans.begin(), ans.end()};
-            return anss;
+           }
+            
+            return count;
+    
         }
     };
